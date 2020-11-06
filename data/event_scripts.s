527b689b9189844af800ff463d12e02635dfc4ff
Chance Arthur <hello@chancearthur.com>
2020-11-06 17:41:33 -0500

    POKéNAV -> PokéNav

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -887,7 +887,7 @@ gText_PlayerWhitedOut:: @ 8272DE3
 
 gText_RegisteredTrainerinPokeNav:: @ 8272E0F
 	.string "Registered {STR_VAR_1} {STR_VAR_2}\n"
-	.string "in the POKéNAV.$"
+	.string "in the PokéNav.$"
 
 gText_ComeBackWithSecretPower:: @ 8272E30
 	.string "Do you know the TM SECRET POWER?\p"
